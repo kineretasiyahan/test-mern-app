e23574983b81fbe19cbe5ddfcf189ce4e41b4036
kineretasiyahan <kineret9688@gmail.com>
2021-08-12 22:55:56 +0300

    add change

--- Server/server.js
+++ Server/server.js
@@ -20,3 +20,10 @@ app.listen(PORT,(error)=>{
 
 app.use('/user',userRouter)
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../client/build')))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'../client/build','index.html'))
+    })
+}
+
