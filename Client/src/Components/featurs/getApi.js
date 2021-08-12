03bde7e57959cff0ec2475e6935b22ecc32fd4e5
kineretasiyahan <kineret9688@gmail.com>
2021-08-12 23:22:48 +0300

    add change

--- Client/src/Components/featurs/getApi.js
+++ Client/src/Components/featurs/getApi.js
@@ -1,10 +1,10 @@
-import { useState } from "react"
+// import { useState } from "react"
 
-export default function GetApi(){
-    const [data,setData] = useState("")
-    fetch("http://localhost:8080/")
-    .then(res=>{return res})
-    .then(result=>{return setData(result)})
-    .catch(error=>{return(error)})
-    return data;
-}
+// export default function GetApi(){
+//     const [data,setData] = useState("")
+//     fetch("http://localhost:8080/")
+//     .then(res=>{return res})
+//     .then(result=>{return setData(result)})
+//     .catch(error=>{return(error)})
+//     return data;
+// }
